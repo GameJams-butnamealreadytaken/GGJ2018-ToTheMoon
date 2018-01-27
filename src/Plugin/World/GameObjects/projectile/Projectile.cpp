ff6d4a936dc0adfcf2d4bd90586f3448cf0795cf
rg25 <r.grimmer@laposte.net>
2018-01-27 02:45:08 +0100

    Added Projectile

---
+++ src/Plugin/World/GameObjects/projectile/Projectile.cpp
@@ -0,0 +1,42 @@
+#include "Projectile.h"
+
+/**
+ * @brief Constructor
+ */
+Projectile::Projectile(ShEntity2 * pEntity, const CShVector2 & vPosition)
+: GameObject(pEntity, vPosition)
+{
+
+}
+
+/**
+ * @brief Destructor
+ */
+Projectile::~Projectile(void)
+{
+	// ...
+}
+
+/**
+* @brief Initialize
+*/
+void Projectile::Initialize(void)
+{
+
+}
+
+/**
+* @brief Release
+*/
+void Projectile::Release(void)
+{
+
+}
+
+/**
+* @brief Start
+*/
+void Projectile::Start(const CShVector2 & vPosition, const CShVector2 & vDestination, const CShVector2 & vSpeed)
+{
+
+}
