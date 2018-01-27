00fb13a85c93d61e61aed79bec14c8718eb52a7f
Johann NOVAK <johann.novak25@gmail.com>
2018-01-27 21:21:40 +0100

    Resources : updated font | GameStateGame : added dialog for exiting game

--- src/Game/GameStates/GameStateGame.h
+++ src/Game/GameStates/GameStateGame.h
@@ -7,27 +7,35 @@ class GameStateGame : public GameState
 
 public:
 
-	explicit	GameStateGame		(void);
-	virtual		~GameStateGame		(void);
+	explicit			GameStateGame		(void);
+	virtual				~GameStateGame		(void);
 
-	void		init				(void) SH_ATTRIBUTE_OVERRIDE;
-	void		release				(void) SH_ATTRIBUTE_OVERRIDE;
+	void				init				(void) SH_ATTRIBUTE_OVERRIDE;
+	void				release				(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void		entered				(void) SH_ATTRIBUTE_OVERRIDE;
-	void		exiting				(void) SH_ATTRIBUTE_OVERRIDE;
+	void				entered				(void) SH_ATTRIBUTE_OVERRIDE;
+	void				exiting				(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void		obscuring			(void) SH_ATTRIBUTE_OVERRIDE;
-	void		revealed			(void) SH_ATTRIBUTE_OVERRIDE;
+	void				obscuring			(void) SH_ATTRIBUTE_OVERRIDE;
+	void				revealed			(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void		update				(float dt) SH_ATTRIBUTE_OVERRIDE;
+	void				update				(float dt) SH_ATTRIBUTE_OVERRIDE;
 
-	void		touchBegin			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
-	void		touchEnd			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
-	void		touchMove			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
+	void				touchBegin			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
+	void				touchEnd			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
+	void				touchMove			(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
 
 protected:
 
-	void		load				(void);
-	void		unload				(void);
+	void				load				(void);
+	void				unload				(void);
 
+	//
+	// GUI Slots
+	static bool			OnButtonClickedYes	(ShGUIControlButton * pButton);
+	static bool			OnButtonClickedNo	(ShGUIControlButton * pButton);
+	
+private:
+
+	ShGUIModalDialog *	m_pQuitDialog;
 };
